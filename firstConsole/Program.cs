00eced4e6adb676f833d85081edc8a0dc29c07fd
azick7553 <azick7553@gmail.com>
2021-01-19 19:33:22 +0500

    first commit

---
+++ firstConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace firstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y;
+            string s = "Hello";
+            string e = "World";
+
+            System.Console.WriteLine(s + " " + e);
+            System.Console.WriteLine($"S: {s},  E:{e}");
+            System.Console.WriteLine(x + 5);
+
+
+            //+ - / *
+            //int z = 0;
+
+            // double
+            // string
+            // char
+            // bool
+
+
+            Console.WriteLine("first");
+            Console.WriteLine("first");
+        }
+    }
+}
